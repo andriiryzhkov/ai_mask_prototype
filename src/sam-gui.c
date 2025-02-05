19fb510573ee18ef50a48aaf8038b5f510b542a7
Andrii Ryzhkov <andrii.ryzhkov@gmail.com>
2025-02-05 23:06:36 +0100

    Remove gui header

--- src/sam-gui.c
+++ src/sam-gui.c
@@ -1,9 +1,41 @@
-#include "sam-gui.h"
+#include "sam-c.h"
+
 #include <string.h>
+#include <gtk/gtk.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
+typedef struct {
+    float x;        // x coordinate on original image
+    float y;        // y coordinate on original image
+    int label;      // 1 for positive, 0 for negative
+} click_point;
+
+typedef struct {
+    // Image data
+    char* image_filename;
+    int image_width;
+    int image_height;
+    GdkPixbuf* image_pixbuf;
+    double image_scale;
+
+    // Click points
+    GArray* points;  // Array of click_point structures
+
+    // SAM model data
+    sam_context_t* sam_ctx;
+    sam_params_t sam_params;
+    sam_image_t* current_image;
+    sam_image_t* mask;
+
+    // GUI elements
+    GtkWidget* window;
+    GtkWidget* drawing_area;
+    GtkWidget* load_button;
+    GtkWidget* save_button;
+} app_context;
 
 static void app_context_init(app_context* ctx) {
     ctx->image_filename = NULL;
